822c08fb44ec27903aa5fd5061b39acf9bfe9b4f
kidow <wcgo2ling@gmail.com>
2022-05-18 23:51:38 +0900

    BackTop z-index

--- src/components/BackTop/index.tsx
+++ src/components/BackTop/index.tsx
@@ -9,7 +9,7 @@ const BackTop: FC<Props> = () => {
   return (
     <button
       onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-      className="fixed right-5 bottom-5 rounded-full bg-neutral-600 p-2 hover:bg-neutral-500 active:bg-neutral-700"
+      className="fixed right-5 bottom-5 z-30 rounded-full bg-neutral-600 p-2 hover:bg-neutral-500 active:bg-neutral-700"
     >
       <ArrowUpIcon className="h-5 w-5" />
     </button>
