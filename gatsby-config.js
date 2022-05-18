31dffa33d7b076e07d0235f431e932fd19a8ede1
kidow <wcgo2ling@gmail.com>
2022-05-18 12:58:32 +0900

    manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,6 +68,60 @@ module.exports = {
       }
     },
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Kidow 블로그',
+        short_name: '블로그',
+        icons: [
+          {
+            src: 'src/images/apple-touch-icon-57x57.png',
+            sizes: '57x57',
+            type: 'image/png'
+          },
+          {
+            src: 'src/images/apple-touch-icon-60x60.png',
+            sizes: '60x60',
+            type: 'image/png'
+          },
+          {
+            src: 'src/images/apple-touch-icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: 'src/images/apple-touch-icon-76x76.png',
+            sizes: '76x76',
+            type: 'image/png'
+          },
+          {
+            src: 'src/images/apple-touch-icon-114x114.png',
+            sizes: '114x114',
+            type: 'image/png'
+          },
+          {
+            src: 'src/images/apple-touch-icon-120x120.png',
+            sizes: '120x120',
+            type: 'image/png'
+          },
+          {
+            src: 'src/images/apple-touch-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'src/images/apple-touch-icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#171717',
+        background_color: '#19191c'
+      }
+    }
   ]
 }
