80302b98a27ef47e92f3731c3d0f0168a2c67b1d
kidow <wcgo2ling@gmail.com>
2022-06-19 23:25:14 +0900

    projects

---
+++ src/data/index.ts
@@ -0,0 +1,23 @@
+export const PROJECTS: Array<{ name: string; url: string; thumbnail: string }> =
+  [
+    {
+      name: 'components.',
+      url: 'https://components.kidow.me',
+      thumbnail: '/components.png'
+    },
+    {
+      name: 'archive.',
+      url: 'https://archive.kidow.me',
+      thumbnail: '/archive.png'
+    },
+    {
+      name: 'opengraph.',
+      url: 'https://opengraph.kidow.me',
+      thumbnail: '/opengraph.png'
+    },
+    {
+      name: 'ddtalk',
+      url: 'https://ddtalk.kr',
+      thumbnail: '/ddtalk.png'
+    }
+  ]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import type { FC } from 'react'
 import { graphql } from 'gatsby'
 import { Card, Footer, SEO } from 'components'
+import { PROJECTS } from 'data'
 
 const HomePage: FC<{
   data: {
@@ -55,6 +56,26 @@ const HomePage: FC<{
             />
           ))}
         </ul>
+        <div className="space-y-5">
+          <h2 className="mt-20 w-40 border-b-2 border-neutral-50 pb-3 text-2xl sm:text-3xl">
+            프로젝트
+          </h2>
+          <ul className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {PROJECTS.map((item, key) => (
+              <li className="group" key={key}>
+                <a href={item.url} target="_blank">
+                  <img
+                    src={item.thumbnail}
+                    alt=""
+                    className="h-40 w-full rounded"
+                    draggable={false}
+                  />
+                  <div className="mt-2 group-hover:underline">{item.name}</div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <Footer path={path} />
     </main>
