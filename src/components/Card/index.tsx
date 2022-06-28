fdfd36c9ed66829b9ed4e12ef5b2147f533024a3
kidow <wcgo2ling@gmail.com>
2022-06-28 12:07:15 +0900

    keyword 색입히기

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'gatsby'
 import React from 'react'
 import type { FC } from 'react'
+import classnames from 'classnames'
 
 export interface Props {
   title: string
@@ -25,7 +26,14 @@ const Card: FC<Props> = ({ title, date, description, keywords, slug }) => {
           <div className="flex flex-wrap gap-3 text-xs md:text-sm">
             {keywords.split(', ').map((keyword) => (
               <span
-                className="rounded-full bg-neutral-800 py-1 px-3"
+                className={classnames('rounded-full bg-neutral-800 py-1 px-3', {
+                  'border border-[#61dafb] text-[#61dafb]': keyword === 'React',
+                  'border border-[#38bdf8] text-[#38bdf8]':
+                    keyword === 'TailwindCSS',
+                  'border border-[#bf4080] text-[#bf4080]': keyword === 'SCSS',
+                  'border border-[#da936a] text-[#da936a]':
+                    keyword === 'Styled Components'
+                })}
                 key={keyword}
               >
                 {keyword}
