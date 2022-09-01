60670214c249bd1de418816730300a2d76c0cde5
kidow <wcgo2ling@gmail.com>
2022-09-01 18:41:13 +0900

    ddtalk 제외

--- src/data/index.ts
+++ src/data/index.ts
@@ -16,11 +16,6 @@ export const PROJECTS: Array<{ name: string; url: string; thumbnail: string }> =
       thumbnail: '/opengraph.png'
     },
     {
-      name: 'ddtalk',
-      url: 'https://ddtalk.kr',
-      thumbnail: '/ddtalk.png'
-    },
-    {
       name: 'Feedbank',
       url: 'https://beta.feedbank.app',
       thumbnail: '/feedbank.png'
