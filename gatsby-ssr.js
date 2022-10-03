94328f1e7facc144bbe05e7ab1304fa17c75000c
kidow <wcgo2ling@gmail.com>
2022-10-03 09:14:04 +0900

    feedbank 이전

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,10 +3,9 @@ import React from 'react'
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <script
-      type="text/javascript"
-      async
-      src="https://beta.cdn.feedbank.app/plugin.js"
-      plugin-key="b4164b86-acd0-4f10-8171-906046cd71ab"
-    ></script>
+      defer
+      src="https://cdn.feedbank.app/plugin.js"
+      plugin-key="fa46598f-aa5e-46fc-be63-2d3e339383c5"
+    />
   ])
 }
