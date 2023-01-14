4752957fee30b3485b0aefd4eab560ea6e203885
kidow <wcgo2ling@gmail.com>
2023-01-14 22:14:46 +0900

    shouldUpdateScroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
 require('prismjs/themes/prism-tomorrow.min.css')
 require('prismjs/plugins/command-line/prism-command-line.css')
 require('./src/styles/index.css')
+
+exports.shouldUpdateScroll = ({ routerProps: { location } }) =>
+  location.pathname.length !== 2
