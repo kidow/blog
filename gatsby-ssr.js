5a228b79023e05359ca3f19012c809154dbdff42
kidow <wcgo2ling@gmail.com>
2023-01-27 17:07:56 +0900

    feedbank cdn 교체

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,9 +3,11 @@ import React from 'react'
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <script
+      type="text/javascript"
+      async
       defer
       src="https://cdn.feedbank.app/plugin.js"
-      plugin-key="fa46598f-aa5e-46fc-be63-2d3e339383c5"
+      plugin-key="53e1b21b-52a5-457c-8209-e4f8f12667ff"
     />
   ])
 }
